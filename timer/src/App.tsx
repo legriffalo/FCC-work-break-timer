893a610b345048182690744d3dcab6d4c8cce102
Nick-legriffalo-Grif <56260217+legriffalo@users.noreply.github.com>
2025-03-30 21:29:31 +0300

    gui progress

--- timer/src/App.tsx
+++ timer/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
     <>
       <Header title = "Break Timer"></Header>
 
-      <div className='w-5/10 m-auto border bg-blue-500 rounded-3xl p-5'>
+      <div className='w-5/10 m-auto border-3 bg-[#333333] rounded-3xl p-5 text-white'>
         <div className='w-9/10 h-[15vh] m-auto flex flex-row justify-center gap-5'>
           <SetUp id = "break-label" info = "Break:" style = "w-4/10 flex justify-center items-center border bg-blue-200 rounded-xl gap-2 p-2" length={90} ></SetUp>
           <SetUp id = "session-label" info = "Session:" style = "w-4/10 text-white flex justify-center items-center border bg-blue-600 rounded-xl gap-2 p-2" length={90} ></SetUp>
